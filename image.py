fed01392c495b6d076bfe3a44e82807d0eda0330
Manu V <132737827+1010manu@users.noreply.github.com>
2023-06-05 20:28:51 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,16 @@
+from rembg import remove
+##from PIL import *
+##inp='download.png'
+##out='out.jpg'
+##input=Image.open(inp)
+##output=remove(input)
+##output.save(out)
+
+
+##inp='silent.jpg'
+##out='out.png'
+##with open(inp,'rb') as i:
+##    with open(out,'wb') as o:
+##        input=i.read()
+##        output=remove(input)
+##        o.write(out)
